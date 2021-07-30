0d699b2477faf065706ad669c01a44d9cc68065b
Niklas Ertle <hockeytertle@gmail.com>
2021-07-30 13:09:21 -0600

    Added functions to get the latitude and longitude of the location searched, then uses those to call again to the API to get the weather at that location. This is done because the API requires latitiude and longitude in order to get the forecast for an area and does not accept city names.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,51 @@
 var searchInput = $('#search-input');
 var searchHistoryEl = $('#search-history-element');
-var currentWeatherEl = $('#current-weather');
-var fiveDayEl = $('#five-day');
+var currentWeatherEl = $('#current-weather-el');
+var fiveDayEl = $('#five-day-el');
 
-var getWeather = function (event) {
+// Gets the lat and lon of the location from Open Weather API
+var getLocation = function (event) {
     event.preventDefault();
     var citySearch = searchInput.val().trim();
-    console.log(citySearch);
+    var searchUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + citySearch + '&appid=ca767b568b7657c6b5f4da781ac95579'
+    searchHistory(citySearch);
+
+    fetch(searchUrl).then(function (response) {
+        if (response === 404) {
+            console.log('404 Error');
+        } else {
+            return response.json();
+        }
+    }).then(function (data) {
+        var lat = data.coord.lat;
+        var lon = data.coord.lon;
+        getWeather(lat, lon);
+    });
 };
 
-$('#form-el').submit(getWeather);
+// Uses the lat and lon to get the forecast for the location searched
+function getWeather(lat, lon) {
+    var searchUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&appid=ca767b568b7657c6b5f4da781ac95579';
 
-// Fetch data for searched city from the weather API
+    fetch(searchUrl).then(function (response) {
+        if (response === 404) {
+            console.log('404 Error');
+        } else {
+            return response.json();
+        }
+    }).then(function (data) {
+        console.log(data);
+    });
+};
+
 // Save search to localStorage, delete at 10 cities
 // Display search history from localStorage, buttons to be able to search again
+function searchHistory(city) {
+    console.log(city);
+
+}
+
+$('#form-el').submit(getLocation);
+
 // Display current weather
 // Display 5 day forecast
